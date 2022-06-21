ed6e4b93fa9da129d6baa9a52d5f7d55d5971bd7
Le The Tiem <19522330@gm.uit.edu.vn>
2022-06-21 10:27:37 +0700

    Event handler

--- starter/script.js
+++ starter/script.js
@@ -142,5 +142,23 @@ btnScrollTo.addEventListener('click', function (e) {
   //   behavior: 'smooth',
   // });
 
-  section1.scrollIntoView({behavior: 'smooth'})
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+//189. Type of event and event handler
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000)
+
+// h1.onmouseenter = function(e){
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// }
+
+//190.Event Propagation
